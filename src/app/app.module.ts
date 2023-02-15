6aaf5f16eaa118bd626f3e840447cd6ffd1a89f8
OliveiraJess <jessica-lm-@hotmail.com>
2023-02-15 00:37:06 -0300

    feat: criando primeiro component form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTransferenciaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+
+export class NovaTransferenciaComponent {
+
+}

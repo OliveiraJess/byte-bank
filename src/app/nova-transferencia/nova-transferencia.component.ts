f786dc04915c4d74e44925d92d5a4751fd99ac6d
OliveiraJess <jessica-lm-@hotmail.com>
2023-02-22 17:16:23 -0300

    feat: adicionando FormsModulo ao app.module e pegando os valores do input com a funcão transferir

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector:'app-nova-transferencia',
+  selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
   styleUrls: ['./nova-transferencia.component.scss']
 })
 
 export class NovaTransferenciaComponent {
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('solicitado nova transferencia');
+    console.log('Valor: ', this.valor);
+    console.log('Destino: ', this.destino);
+
+  }
 
 }

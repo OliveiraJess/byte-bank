b4f1b377df672de93a48b0e8e9043a15680cd28a
OliveiraJess <jessica-lm-@hotmail.com>
2023-03-01 16:53:55 -0300

    feat: novo componente extrato

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-extrato',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./extrato.component.scss']
 })
 export class ExtratoComponent {
+  @Input() transferencia: any;
 
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
 }
